55485ed01e2f9329b855a16c8ad87f77c7044b05
anchal79 <anchaljainapure79@gmail.com>
2024-09-10 22:30:45 +0530

    question 2

--- ans.java
+++ ans.java
@@ -1,7 +1,17 @@
 
 import java.util.*;
 public class ans {
-    public static void main{
-        System.out.println("Hello World!!");
+    public static int findMissingNumber(int[] arr, int n) {
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        for (int num : arr) {
+            actualSum += num;
+        }
+        return expectedSum - actualSum;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5};
+        int n = 5;
+        System.out.println("Missing number: " + findMissingNumber(arr, n)); // Output: 3
     }
 }
